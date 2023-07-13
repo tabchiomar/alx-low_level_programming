52c7f7d637e0898f7885dfafafeb68d3cfe775e3
tabchiomar <tabchi.omar@gmail.com>
2023-07-13 15:52:16 -0700

    end commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int d=sizeof(char);
 int f=sizeof(float);
 
 printf("Size of a char: %d byte(s)\n", d);
-printf("Size of a int: %d byte(s)\n", a);
+printf("Size of an int: %d byte(s)\n", a);
 printf("Size of a long int: %d byte(s)\n", b);
 printf("Size of a long long int: %d byte(s)\n", c);
 printf("Size of a float: %d byte(s)\n", f);
